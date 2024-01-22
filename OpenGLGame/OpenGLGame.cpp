8c2d4c33ad60014a0d9463aea9e9517e0fac01da
Hugo Lundblad <hugo01.lundblad@gmail.com>
2024-01-22 16:07:00 +0100

    "Hello Window"

--- OpenGLGame/OpenGLGame.cpp
+++ OpenGLGame/OpenGLGame.cpp
@@ -1,10 +1,56 @@
-
-
 #include <iostream>
+#include <windows.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void error_callback(int error, const char* msg) {
+    string s;
+    s = " [" + to_string(error) + "] " + msg + '\n';
+    cerr << s << endl;
+}
+
+int main() {
+    glfwInit();
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        cout << "Failed to init GLFW" << endl;
+        return -1;
+    }
+
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, NULL);
+    if (window == nullptr)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
