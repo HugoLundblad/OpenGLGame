9877d90949da4e8d2c2fd7b441bcfac628e26c23
Hugo Lundblad <hugo01.lundblad@gmail.com>
2024-01-23 16:04:46 +0100

    Started adding vertices for the triangle

--- OpenGLGame/OpenGLGame.cpp
+++ OpenGLGame/OpenGLGame.cpp
@@ -6,30 +6,39 @@
 
 using namespace std;
 
+void processInput(GLFWwindow*);
+
 void error_callback(int error, const char* msg) {
     string s;
     s = " [" + to_string(error) + "] " + msg + '\n';
     cerr << s << endl;
 }
 
+struct Window {
+    void Render() {
+
+    }
+};
+
 int main() {
-    glfwInit();
 
     glfwSetErrorCallback(error_callback);
 
-    if (!glfwInit())
-    {
+    // Initialize GLFW
+    if (!glfwInit()) { // Exit, if it failed
         cout << "Failed to init GLFW" << endl;
         return -1;
     }
 
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, NULL);
+    // Request Window from Operating System
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
     if (window == nullptr)
     {
         cout << "Failed to create GLFW window" << endl;
@@ -38,19 +47,57 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
+    // Initialize GLAD (connects OpenGL Functions)
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         cout << "Failed to initialize GLAD" << endl;
+        glfwTerminate();
         return -1;
     }
     glViewport(0, 0, 800, 600);
 
+    // Initialization ends here
+    // ==================================================================
+    // The Real Program starts here
+    float red = 0;
+
+    // While the User doesn't want to Quit (X Button, Alt+F4)
     while (!glfwWindowShouldClose(window))
     {
-        glfwSwapBuffers(window);
+        // process input (e.g. close window on Esc)
         glfwPollEvents();
-    }
+        processInput(window);
+        red += 0.0001f;
+        if (red > 1)
+        {
+            red -= 1;
+        }
 
+        float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+        };
+
+        unsigned int VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // render (paint the current frame of the game)
+        glClearColor(red, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // present (send the current frame to the computer screen)
+        glfwSwapBuffers(window); // ??
+    }
+    // Cleans up all the GLFW stuff
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
